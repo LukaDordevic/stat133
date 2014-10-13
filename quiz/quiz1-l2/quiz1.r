057c0fa098800d5eda69129afc5ebf293b519f29
Karl Kumbier <kkumbier@berkeley.edu>
2014-10-13 11:38:53 -0700

    add quiz

--- quiz/quiz1-l2/quiz1.r
+++ quiz/quiz1-l2/quiz1.r
@@ -2,12 +2,13 @@
 # required to perform the following tasks. The dataset includes data for houses
 # in the city of Berkeley.
 
+load("SFHousing-2.rda")
 
 # calculate the mean and median bsqft of houses in Berkeley. Store these as the
 # variables <mean.bsqft> and <med.bsqft> respectively.
 
-# mean.bsft <- your code here
-# med.bsqft <- your code here
+mean.bsqft <- mean(housing$bsqft, na.rm=T)
+med.bsqft <- median(housing$bsqft, na.rm=T)
 
 
 # For each house in the dataset, calculate the squared difference between its
@@ -15,7 +16,7 @@
 # <bsqft.diffs>. Note that this should be a numeric vector with length equal to
 # the number of observations in the dataset
 
-# bsqft.diffs <- your code here
+bsqft.diffs <- (housing$bsqft - med.bsqft)^2
 
 
 
@@ -24,15 +25,18 @@
 # 1) houses whose bsqft is strictly greater than <mean.bsqft>:  <bsft.greater>
 # 2) houses whose bsqft is less than or equal to  <mean.bsqft>: <bsqft.less>
 
-# bsqft.greater <- your code here
-# bsqft.less <- your code here
+
+
+
+bsqft.greater <- housing[housing$bsqft>mean.bsqft,]
+bsqft.less <- housing[housing$bsqft<mean.bsqft,]
 
 
 # For each of your subsets, create a vector giving the price of each house. Name
 # these variables <rooms.greater.price> and <rooms.less.price>.
 
-# rooms.greater.price <- your code here
-# rooms.less.price <- your code here
+rooms.greater.price <- as.vector(bsqft.greater$price)
+rooms.less.price <- as.vector(bsqft.less$price)
 
 
 
@@ -48,11 +52,17 @@
 # Your function should return the average of <prices> for all observations with
 # <br> in the range (inclusive) specified by <room.range>
 
-priceByRooms <- function(room.range, br, prices) {
+priceByRooms <- function(room.range, br, price) {
+      x=c()
+        for(i in 1:length(br)){
+                if( room.range[1] < br[i] & br[i] < room.range[2] )
+                          x=c(x, price[i])
+            }
 
-    # your code here
+        return(mean(x, na.rm=T))
 
-}
+
+  }
 
 
 # Please create a plot of house price (y-axis) against br (x-axis). Your plot
@@ -61,3 +71,5 @@ priceByRooms <- function(room.range, br, prices) {
 # 2) axis labels: "price" and "number of rooms"
 # 3) plotting character set to 20
 
+
+plot(housing$price ~ housing$br, cex=0.2, main="Housing price vs Number of rooms", xlab="Number of rooms", ylab="price", na.rm=T)
